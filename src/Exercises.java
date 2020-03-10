c52d3be25dbfea73bc3938edc4c7544d10b267dc
Emily <efg12103@gmail.com>
2020-03-10 09:11:53 -0400

    exercise 5

--- src/Exercises.java
+++ src/Exercises.java
@@ -73,35 +73,79 @@ public class Exercises {
     }
 
     public int[] bubble(int[] list, boolean ascending) {
-        return null;
+        if (list == null) {
+            return null;
+        }
+        if (ascending) {
+            boolean swapped = true;
+            while (swapped) {
+                swapped = false;
+                for (int i = 1; i < list.length - 1; i++) {
+                    if (list[i] > list[i + 1]) {
+                        int temp = list[i];
+                        list[i] = list[i + 1];
+                        list[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            }
+        } else {
+            boolean swapped = true;
+            while (swapped) {
+                swapped = false;
+                for (int i = 1; i < list.length - 1; i++) {
+                    if (list[i] < list[i + 1]) {
+                        int temp = list[i];
+                        list[i] = list[i + 1];
+                        list[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            } 
+        }
+        return list;
     }
 
     public ArrayList<String> bubble(ArrayList<String> list, boolean ascending) {
-        return null;
+        if (list == null) {
+            return null;
+        }
     }
 
     public ArrayList<Integer> insertion(ArrayList<Integer> list, boolean ascending) {
-        return null;
+        if (list == null) {
+            return null;
+        }
     }
 
     public String[] insertion(String[] list, boolean ascending) {
-        return null;
+        if (list == null) {
+            return null;
+        }
     }
 
     public int[] selection(int[] list, boolean ascending) {
-        return null;
+        if (list == null) {
+            return null;
+        }
     }
 
     public ArrayList<String> selection(ArrayList<String> list, boolean ascending) {
-        return null;
+        if (list == null) {
+            return null;
+        }
     }
 
     public ArrayList<Integer> merge(ArrayList<Integer> list, boolean ascending) {
-        return null;
+        if (list == null) {
+            return null;
+        }
     }
 
     public String[] merge(String[] list, boolean ascending) {
-        return null;
+        if (list == null) {
+            return null;
+        }
     }
 
     private boolean hasNull(String[] strings) {
