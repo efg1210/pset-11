92a08e646391a4e9ab917801b9d80d59fd93180f
Emily <efg12103@gmail.com>
2020-03-10 08:39:40 -0400

    formatting

--- src/Exercises.java
+++ src/Exercises.java
@@ -2,77 +2,77 @@ import java.util.ArrayList;
 
 public class Exercises {
 
-  public int findMe(int[] list, int target) {
-    if (list == null) {
-      return -1;
-    }
-    for (int i = 0; i < list.length; i++) {
-        if (list[i] == target) {
-            return i;
+    public int findMe(int[] list, int target) {
+        if (list == null) {
+            return -1;
         }
-    }
-    return -1;
-  }
-
-  public int findMe(ArrayList<String> list, String target) {
-    if (list == null || hasNull(list)) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] == target) {
+                return i;
+            }
+        }
         return -1;
     }
-    for (int i = 0; i < list.length; i++) {
-        if (list[i].equals(target)) {
-            return i;
+
+    public int findMe(ArrayList<String> list, String target) {
+        if (list == null || hasNull(list)) {
+            return -1;
         }
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].equals(target)) {
+                return i;
+            }
+        }
+        return -1;
     }
-    return -1;
-  }
 
-  public int findMeFaster(ArrayList<Integer> list, int target) {
-    
-    return -1;
-  }
+    public int findMeFaster(ArrayList<Integer> list, int target) {
 
-  public int findMeFaster(String[] list, String target) {
-    return -1;
-  }
-
-  public int[] bubble(int[] list, boolean ascending) {
-    return null;
-  }
-
-  public ArrayList<String> bubble(ArrayList<String> list, boolean ascending) {
-    return null;
-  }
-
-  public ArrayList<Integer> insertion(ArrayList<Integer> list, boolean ascending) {
-    return null;
-  }
-
-  public String[] insertion(String[] list, boolean ascending) {
-    return null;
-  }
-
-  public int[] selection(int[] list, boolean ascending) {
-    return null;
-  }
-
-  public ArrayList<String> selection(ArrayList<String> list, boolean ascending) {
-    return null;
-  }
-
-  public ArrayList<Integer> merge(ArrayList<Integer> list, boolean ascending) {
-    return null;
-  }
-
-  public String[] merge(String[] list, boolean ascending) {
-    return null;
-  }
-
-  private boolean hasNull(String[] strings) {
-    for (String string: strings) {
-      if (string == null) {
-        return true;
-      }
+        return -1;
     }
-    return false;
-  }
+
+    public int findMeFaster(String[] list, String target) {
+        return -1;
+    }
+
+    public int[] bubble(int[] list, boolean ascending) {
+        return null;
+    }
+
+    public ArrayList<String> bubble(ArrayList<String> list, boolean ascending) {
+        return null;
+    }
+
+    public ArrayList<Integer> insertion(ArrayList<Integer> list, boolean ascending) {
+        return null;
+    }
+
+    public String[] insertion(String[] list, boolean ascending) {
+        return null;
+    }
+
+    public int[] selection(int[] list, boolean ascending) {
+        return null;
+    }
+
+    public ArrayList<String> selection(ArrayList<String> list, boolean ascending) {
+        return null;
+    }
+
+    public ArrayList<Integer> merge(ArrayList<Integer> list, boolean ascending) {
+        return null;
+    }
+
+    public String[] merge(String[] list, boolean ascending) {
+        return null;
+    }
+
+    private boolean hasNull(String[] strings) {
+        for (String string: strings) {
+            if (string == null) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
